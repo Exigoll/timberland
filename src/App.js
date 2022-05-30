f095f860a735d91343319ac461f6e4beed2d8f01
Exigoll <exigoll@bk.ru>
2022-05-30 22:05:45 +0500

    init: street beat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/app.scss";
+import logo from "./assets/svgicons/logo.svg";
+import cart from "./assets/svgicons/cart.svg";
+import favorites from "./assets/svgicons/favorites.svg";
+import user from "./assets/svgicons/user.svg";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="container">
+        <header className="header">
+          <div className="header__logo logoHeader">
+            <img className="logoHeader__img" src={logo}></img>
+            <div className="logoHeader__title titleLogo">
+              <h1 className="titleLogo__title">street beat</h1>
+              <h2 className="titleLogo__subtitle">Only original products</h2>
+            </div>
+          </div>
+          <div className="header__menu menuHeader">
+            <button className="menuHeader__cart" type="submit">
+              <img src={cart}></img>
+              <span>12 499 руб.</span>
+            </button>
+            <button className="menuHeader__favorites" type="submit">
+              <img src={favorites}></img>
+            </button>
+            <button className="menuHeader__user" type="submit">
+              <img src={user}></img>
+            </button>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
